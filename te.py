91fd37e067df9b12a0859360daa11e3c7ec2a39d
malai12 <thirumohan2001@gmail.com>
2021-01-10 20:16:12 +0530

    smile

---
+++ te.py
@@ -0,0 +1,20 @@
+# Driver program
+arr = [50, 40, 70, 60, 90]
+index = [3, 0, 4, 1, 2]
+n = len(arr)
+temp = [0] * n
+print(temp)
+ 
+# arr[i] should be
+    # present at index[i] index
+for i in range(0,n):
+    temp[index[i]] = arr[i]
+    print(index[i],end=" ")
+print(temp)
+# Copy temp[] to arr[]
+for i in range(0,n):
+    arr[i] = temp[i]
+    print(temp[i],end=" ")
+    index[i] = i
+    
+    
